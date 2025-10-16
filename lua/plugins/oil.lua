142d4457f3397b2a24d9aeb21fb5198b71105a14
Jordy <jordyslagter@icloud.com>
2025-10-16 15:15:37 +0200

    make oil skip confirmation on simple edits

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -10,6 +10,7 @@ return {
       view_options = {
         show_hidden = true,
       },
+      skip_confirm_for_simple_edits = true,
     },
     keys = {
       -- Open Oil in the current directory
