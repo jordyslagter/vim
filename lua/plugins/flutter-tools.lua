11c2c56a0b5ab832455bcf5dcba5f9d5f739d5f6
Jordy <jordyslagter@icloud.com>
2025-10-16 15:27:39 +0200

    add dart/flutter support

---
+++ lua/plugins/flutter-tools.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "nvim-flutter/flutter-tools.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    ft = "dart",
+    config = true,
+  },
+}
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,16 @@
+-- package list:
+-- https://mason-registry.dev/registry/list
+
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      -- before adding anything to mason via this method, check if it is available
+      -- as a lazyvim extra first and use that instead
+      ensure_installed = {
+        -- dart
+        "dart-debug-adapter",
+      },
+    },
+  },
+}
