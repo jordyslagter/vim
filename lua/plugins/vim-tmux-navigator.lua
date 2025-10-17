9e8bed7e3a522ef0e7dce3d0ce5ec03db2c6c309
Jordy <jordyslagter@icloud.com>
2025-10-17 12:38:23 +0200

    add vim tmux navigator

---
+++ lua/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>", desc = "Navigate to left pane" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>", desc = "Navigate to down pane" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>", desc = "Navigate to up pane" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>", desc = "Navigate to right pane" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>", desc = "Navigate to previous pane" },
+    },
+  },
+}
