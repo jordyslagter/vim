a95692b8113c6487cd61dc610b4f99b71e667df9
Jordy <jordyslagter@icloud.com>
2025-10-17 18:41:00 +0200

    add more file types for emmet

--- lua/plugins/nvim-emmet.lua
+++ lua/plugins/nvim-emmet.lua
@@ -1,6 +1,23 @@
 return {
   {
     "olrtg/nvim-emmet",
-    ft = "html",
+    ft = {
+      -- filetypes from: https://github.com/microsoft/vscode-docs/blob/main/docs/languages/emmet.md
+      "html",
+      "haml",
+      "pug",
+      "slim",
+      "jsx",
+      "tsx",
+      "xml",
+      "xsl",
+      "css",
+      "scss",
+      "sass",
+      "less",
+      "stylus",
+      "handlebars",
+      "php",
+    },
   },
 }
