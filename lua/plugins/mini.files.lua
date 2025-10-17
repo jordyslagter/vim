b9fc282edc1f0bc45a8d64025b7ec93f0870bd8d
Jordy <jordyslagter@icloud.com>
2025-10-17 11:47:21 +0200

    fix mini files not opening in current directory

--- lua/plugins/mini.files.lua
+++ lua/plugins/mini.files.lua
@@ -8,7 +8,7 @@ return {
       {
         "-",
         function()
-          require("mini.files").open()
+          require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
         end,
         desc = "Open mini.files (Directory of Current File)",
       },
