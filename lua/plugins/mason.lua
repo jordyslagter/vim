5d19132c7688b8abc07eb62ffc98276dcb98eb46
Jordy <jordyslagter@icloud.com>
2025-10-21 12:39:51 +0200

    use new lazyvim extra for dart

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -8,8 +8,6 @@ return {
       -- before adding anything to mason via this method, check if it is available
       -- as a lazyvim extra first and use that instead
       ensure_installed = {
-        -- dart
-        "dart-debug-adapter",
         -- html
         "emmet-language-server",
       },
