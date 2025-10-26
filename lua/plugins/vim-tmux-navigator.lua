dc6bc113e2a13de5f4b6d9a6866076b3f0102417
Jordy <jordyslagter@icloud.com>
2025-10-26 20:06:55 +0100

    feat(vim-tmux-navigator): add Vim Tmux Navigator in brackets in the description of commands to stay consitent with LazyVim standards

--- lua/plugins/vim-tmux-navigator.lua
+++ lua/plugins/vim-tmux-navigator.lua
@@ -11,14 +11,30 @@ return {
       "TmuxNavigatorProcessList",
     },
     keys = {
-      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>", desc = "Go to Left Pane" },
-      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>", desc = "Go to Lower Pane" },
-      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>", desc = "Go to Upper Pane" },
-      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>", desc = "Go to Right Pane" },
+      {
+        "<c-h>",
+        "<cmd><C-U>TmuxNavigateLeft<cr>",
+        desc = "Go to Left Pane (Vim Tmux Navigator)",
+      },
+      {
+        "<c-j>",
+        "<cmd><C-U>TmuxNavigateDown<cr>",
+        desc = "Go to Lower Pane (Vim Tmux Navigator)",
+      },
+      {
+        "<c-k>",
+        "<cmd><C-U>TmuxNavigateUp<cr>",
+        desc = "Go to Upper Pane (Vim Tmux Navigator)",
+      },
+      {
+        "<c-l>",
+        "<cmd><C-U>TmuxNavigateRight<cr>",
+        desc = "Go to Right Pane (Vim Tmux Navigator)",
+      },
       {
         "<c-\\>",
         "<cmd><C-U>TmuxNavigatePrevious<cr>",
-        desc = "Go to Previous Pane",
+        desc = "Go to Previous Pane (Vim Tmux Navigator)",
       },
     },
   },
