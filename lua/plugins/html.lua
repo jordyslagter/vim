47bf356b9ab361440c307f5b6289063a0f71618b
Jordy <jordyslagter@icloud.com>
2025-10-27 10:07:51 +0100

    feat(html): stop using nvim-emmet
    the language server is sufficient and nvim-emmet lazy loading requires
    relatively advanced configuration

--- lua/plugins/html.lua
+++ lua/plugins/html.lua
@@ -7,29 +7,4 @@ return {
       },
     },
   },
-  {
-    "olrtg/nvim-emmet",
-    ft = {
-      -- filetypes from: https://github.com/microsoft/vscode-docs/blob/main/docs/languages/emmet.md
-      "html",
-      "haml",
-      "pug",
-      "slim",
-      "jsx",
-      "xml",
-      "xsl",
-      "css",
-      "scss",
-      "sass",
-      "less",
-      "stylus",
-      "handlebars",
-      "php",
-      -- personal additions
-      "js",
-      "tsx",
-      "svelte",
-      "vue",
-    },
-  },
 }
