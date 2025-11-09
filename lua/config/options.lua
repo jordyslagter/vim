541dc9c2d67da11909b2b49ab0684f49d3f31b52
Jordy Slagter <jordyslagter@icloud.com>
2025-11-09 20:38:46 +0100

    feat(tex)!: use skim as the tex viewer

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,10 @@
 -- Add any additional options here
 
 vim.g.snacks_animate = false
+
+-- Use Skim as the PDF viewer
+vim.g.vimtex_view_method = "skim"
+
+-- Enable SyncTeX and auto-reload
+vim.g.vimtex_view_skim_sync = 1 -- Enable forward/inverse search
+vim.g.vimtex_view_skim_activate = 0 -- Focus Skim after compiling
